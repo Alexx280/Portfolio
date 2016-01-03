0a26383389280d83afa4147c491ee5b149da69b1
Alex <gs455@mail.ru>
2016-01-03 20:40:48 +0600

    Two background page

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset='UTF-8'>
+    <link rel='stylesheet' type='text/css' href='design.css'/>
+</head>
+<div id="first"></div>
+<div id="second"></div>
+
+</html>
+
+
+
+<?php
+
+?>
+/**
+ * Created by PhpStorm.
+ * User: busygin
+ * Date: 31.12.2015
+ * Time: 10:37
+ */
