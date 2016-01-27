8320bb20f6cb16b4d73d794e9d2fd3555717c730
Alexx280 <Alexx280@yandex.ru>
2016-01-27 16:16:03 +0600

    Three picture on body, color button, contacts, my name.

--- index.php
+++ index.php
@@ -5,8 +5,38 @@
     <meta charset='UTF-8'>
     <link rel='stylesheet' type='text/css' href='design.css'/>
 </head>
-<div id="first"></div>
+<div id = "top" class="myElement">
+    <p class="contact" style="margin-left: 110px; margin-top: 5px">Бусыгин Алексей</p>
+    <div class="button"></div>
+</div>
+<div id="first">
+      <div class="button-head">
+            <div class="main-container">
+                <section>
+
+                    <a href="https://github.com/Alexx280"> <button class="btn btn-green btn-fill-vert button">GitHub</button></a>
+                    <a href="http://www.freecodecamp.com/alexx280"><button class="btn btn-blue btn-fill-vert button">FreeCodeCamp</button></a>
+                    <a href="http://tomsk.hh.ru/resume/3bbf0c1cff0259bdfb0039ed1f373562374b56"><button class="btn btn-purple btn-fill-vert  button">Resume</button> </a>
+                    <a href="http://www.intuit.ru/verifydiplomas/100752444"><button class="btn btn-navy btn-fill-vert button">Intuit</button></a>
+                    <button class="btn btn-orange btn-fill-vert button">Carrot</button>
+                    <button class="btn btn-red btn-fill-vert button">Alizarin</button>
+                </section>
+            </div>
+      </div>
+</div>
 <div id="second"></div>
+<div id="third">
+  <div id="contact">
+   <p class="contact">
+    Contact with me by <br>
+    E-mail: gs455@mail.ru<br>
+    Skype: white_cefiro<br>
+    Phone: +79539204705<br>
+    </p>
+  </div>
+</div>
+
+
 
 </html>
 
