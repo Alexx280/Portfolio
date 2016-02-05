04e3abdbfe78cb6a493dbc8b5124c74babe635ed
Alexx280 <Alexx280@yandex.ru>
2016-02-05 12:43:46 +0600

    add button "My contsacts"

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <p class="contact" style="margin-left: 110px; margin-top: 5px">Busygin Alexey</p>
     <div id="top_button_about_me" class="button_top">About me</div>
     <div id="top_button_my_works" class="button_top">My works</div>
+    <div id="top_button_my_contacts" class="button_top">My contacts</div>
 </div>
 <div id="first">
       <div class="button-head">
@@ -38,7 +39,32 @@
   </div>
 </div>
 
+<script>
+    var linkNav = document.querySelectorAll('[href^="#nav"]'),
+        V = 2;  // скорость, может иметь дробное значение через точку
+    for (var i = 0; i < linkNav.length; i++) {
+        linkNav[i].addEventListener('click', function(e) {
+            e.preventDefault();
+            var w = window.pageYOffset,  // прокрутка
+                hash = this.href.replace(/[^#]*(.*)/, '$1');  // id элемента, к которому нужно перейти
+            t = document.querySelector(hash).getBoundingClientRect().top,  // отступ от окна браузера до id
+                start = null;
+            requestAnimationFrame(step);  // подробнее про функцию анимации [developer.mozilla.org]
+            function step(time) {
+                if (start === null) start = time;
+                var progress = time - start,
+                    r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
+                window.scrollTo(0,r);
+                if (r != w + t) {
+                    requestAnimationFrame(step)
+                } else {
+                    location.hash = hash  // URL с хэшем
+                }
+            }
+        }, false);
+    }
 
+</script>
 
 </html>
 
