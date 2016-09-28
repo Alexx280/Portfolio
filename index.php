12591728b107d41c66e1285b53cd7ab09076cd05
Alexx280 <gs455@mail.ru>
2016-09-28 10:53:38 +0700

    Новое фото, убраны кнопки, добавлен 3й проект

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 <div id="first" class="fon">
       <div class="button-head">
             <div class="main-container">
-                <section>
+                <!--<section>
 
                     <a href="https://github.com/Alexx280"> <button class="btn btn-green btn-fill-vert button">GitHub</button></a>
                     <a href="http://www.freecodecamp.com/alexx280"><button class="btn btn-blue btn-fill-vert button">FreeCodeCamp</button></a>
@@ -26,16 +26,17 @@
                     <a href="http://www.intuit.ru/verifydiplomas/100752444"><button class="btn btn-navy btn-fill-vert button">Intuit</button></a>
                     <button class="btn btn-orange btn-fill-vert button">Carrot</button>
                     <button class="btn btn-red btn-fill-vert button">Alizarin</button>
-                </section>
+                </section>-->
             </div>
       </div>
     <div class="i_am">
         <div class="foto" style="float: left">
-            <img src="pic/I_am.jpg" alt="Моё фото" >
+            <img src="pic/I_am2.jpg" alt="Моё фото" >
         </div>
-        I am beginner web programmer, live in Tomsk.  I study HTML5, CSS3 (less), JavaScript (jQuery), PHP, MySQL, Git scince 2015 year.
-        I looking for vacancy of junior web developer, nonprofit projects or projects without salary for improving my skills.
-        Currently, I make web sites for customers. Below you can see my works. If you need web site contact with me.
+        I am beginner web programmer scince 2015 year, live in Tomsk. I know HTML5 (Jade), CSS3 (less, sass),
+        JavaScript (jQuery), PHP, MySQL, Git. I looking for vacancy of junior web developer, nonprofit projects
+        or projects without salary for improving my skills. Currently, I make web sites for customers.
+        Below you can see my works. If you need web site contact with me.
     </div>
 
 </div>
@@ -45,11 +46,12 @@
         <div class ="main-container" >
             <div class="tiles">
                 <a href="#" class="tile"><img id="project1"  src="pic/Project1.jpg" width="425" height="300"><div class="details"><span class="title">Учебный курс</span><span class="info" style="font-size: 1em">Повышение осведомлённости сотрудников компании в области информационной безопасности: интерактивный учебный материал, тестирование, обработка и сохранение результатов в БД</span></div></a>
-                <a href="http://jsdevelope.myjino.ru" class="tile"><img id="project1"  src="pic/Project2.jpg" width="425" height="300"><div class="details"><span class="title">Коммерческий сайт - визитка</span><span class="info">Сайт доставки сыпучих грузов</span></div></a>
+                <a href="http://jsdevelope.myjino.ru/Kurgan" class="tile"><img id="project1"  src="pic/Project2.jpg" width="425" height="300"><div class="details"><span class="title">Коммерческий сайт - визитка, одна страница</span><span class="info" style="font-size: 1em">Доставка сыпучих грузов</span></div></a>
+                <a href="http://jsdevelope.myjino.ru/Lady" class="tile"><img id="project1"  src="pic/Project3.jpg" width="425" height="300"><div class="details"><span class="title">Коммерческий сайт - несколько сстраниц, включая книгу отзывов</span><span class="info" style="font-size: 1em">Салон красоты (в работе)</span></div></a>
+                <!--<a href="#" class="tile"><img id="project1"  src="pic/Project1.jpg" width="425" height="300"><div class="details"><span class="title">Lorem Ipsum Dolor</span><span class="info">Quisque vel felis lectus donec vitae dapibus magna</span></div></a>
                 <a href="#" class="tile"><img id="project1"  src="pic/Project1.jpg" width="425" height="300"><div class="details"><span class="title">Lorem Ipsum Dolor</span><span class="info">Quisque vel felis lectus donec vitae dapibus magna</span></div></a>
                 <a href="#" class="tile"><img id="project1"  src="pic/Project1.jpg" width="425" height="300"><div class="details"><span class="title">Lorem Ipsum Dolor</span><span class="info">Quisque vel felis lectus donec vitae dapibus magna</span></div></a>
-                <a href="#" class="tile"><img id="project1"  src="pic/Project1.jpg" width="425" height="300"><div class="details"><span class="title">Lorem Ipsum Dolor</span><span class="info">Quisque vel felis lectus donec vitae dapibus magna</span></div></a>
-                <a href="#" class="tile"><img id="project1"  src="pic/Project1.jpg" width="425" height="300"><div class="details"><span class="title">Lorem Ipsum Dolor</span><span class="info">Quisque vel felis lectus donec vitae dapibus magna</span></div></a>
+                <a href="#" class="tile"><img id="project1"  src="pic/Project1.jpg" width="425" height="300"><div class="details"><span class="title">Lorem Ipsum Dolor</span><span class="info">Quisque vel felis lectus donec vitae dapibus magna</span></div></a>-->
             </div>
         </div>
     </div>
